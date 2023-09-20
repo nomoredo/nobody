bd913f363f4ff5b326cbb958aa4b9fe160c879e9
incredimo <aghil@xo.rs>
2023-09-20 11:15:04 +0400

    sdfsdf

---
+++ nothing/INothing.cs
@@ -0,0 +1,9 @@
+﻿namespace nothing;
+
+///# nothing
+/// a file based secret store that stores and retrieves secrets from files
+/// uses protobuf to serialize and deserialize the secrets
+public interface INothing
+{
+    public string Key { get;  }
+}
