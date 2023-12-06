21fd137064f100cb2b4af2f2d7d6e6cc1131bbec
incredimo <aghil@xo.rs>
2023-12-06 09:25:27 +0400

    asdsd

--- core/StringExtCtx.cs
+++ core/StringExtCtx.cs
@@ -1,6 +1,6 @@
 ﻿using nobody.core;
 
-namespace nobody.online;
+namespace nobody.console;
 
 public static class StringExtCtx
 {
@@ -8,6 +8,8 @@ public static class StringExtCtx
     public static bool has(this string key) => Anybody.here.has(key);
     public static void set(this string key, object value) => Anybody.here.set(key, value);
     public static T get_or<T>(this string key, Func<string, T> defaultValue) => Anybody.here.get_or(key, defaultValue);
+
+    public static string get_string(this string key) => Anybody.here.has(key) ? Anybody.here.get<string>(key) : Anybody.here.ask_string<string>(key).get<string>(key);
 
 
 
