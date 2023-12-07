85edb49d811575c93f3f3484ebd7da3b2795d7c8
incredimo <aghil@xo.rs>
2023-12-07 16:22:13 +0400

    utfhtfg

---
+++ nobody/bin/create_trip_request.dart
@@ -0,0 +1,62 @@
+import 'package:nobody/lib.dart';
+
+void main() async {
+  await create_trip_request();
+}
+
+Future create_trip_request() async {
+  var empno = "9711068";
+  var kind = "Job Travel";
+  var subject = "Test";
+  var description = "Test";
+  var start_date = "01.01.2024";
+  var end_date = "01.01.2024";
+  var country = "United Arab Emirates";
+  var airport_city = "Dubai";
+
+  Nobody()
+      .online()
+      .login(Sap.User('amohandas'))
+      .visit(TripRequestOnBehalfOf)
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--Pernr-inner"),
+          empno)
+      .click(Button.WithId(
+          "application-ZFOC_TRIP_FORM-display-component---object--gobtn"))
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--tripType-hiddenInput"),
+          kind)
+      .set(
+          TextArea.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--sub-inner"),
+          subject)
+      .set(
+          TextArea.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--desc-inner"),
+          description)
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--start-inner"),
+          start_date)
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--end-inner"),
+          end_date)
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--tripCntry-inner"),
+          country)
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--tripRsn-inner"),
+          "simply for fun")
+      .set(
+          Input.WithId(
+              "application-ZFOC_TRIP_FORM-display-component---object--loc-inner"),
+          airport_city);
+}
+
+final TripRequestOnBehalfOf =
+    "https://cbs.almansoori.biz/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html?sap-client=800&sap-language=EN%23Shell-home&appState=lean#ZFOC_TRIP_FORM-display&/TripdetailsSet";
--- termo/lib/lib.dart
+++
@@ -1,3 +0,0 @@
-export 'src/show.dart';
-export 'src/termo_base.dart';
-export 'termo.dart';
