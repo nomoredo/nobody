de3cbc03520b55bd85a55f9cdccc2a555e587dd3
incredimo <aghil@xo.rs>
2024-04-23 10:12:12 +0400

    bump to 🚀 1.0.1 and release 🎉

--- nobody/lib/references.dart
+++ nobody/lib/references.dart
@@ -1,9 +1,9 @@
 export 'dart:async';
 export 'dart:io';
 
-export 'package:codegen/codegen.dart';
+export 'package:nogen/nogen.dart';
 export 'package:puppeteer/puppeteer.dart';
-export 'core/core.dart';
 
+export 'core/core.dart';
 export 'lib.dart';
 export 'web/online.future.dart';
